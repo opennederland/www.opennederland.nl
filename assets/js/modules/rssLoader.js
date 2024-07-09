867c8df5b0be0c7f09803a27f4f4f0198cac5828
Johan Groenen <johan@jgroenen.nl>
2024-07-09 13:40:06 +0200

    mapping for json feed

--- assets/js/modules/rssLoader.js
+++ assets/js/modules/rssLoader.js
@@ -7,12 +7,19 @@ export default function () {
         return {
           "title": item.querySelector("title").textContent,
           "link": item.querySelector("link").textContent,
-          "description": item.querySelector("description").textContent,
+          "description": item.querySelector("description").textContent.replace(/<[^>]*>?/gm, '').substring(0, 400).split(' ').slice(0, -1).join(' ') + '...',
           "pubDate": item.querySelector("pubDate").textContent
         }
       });
     } else {
-      return JSON.parse(responseText).items;
+      return JSON.parse(responseText).items.map(item => {
+        return {
+          "title": item.title,
+          "link": item.url,
+          "description": item.content_html.replace(/<[^>]*>?/gm, '').substring(0, 400).split(' ').slice(0, -1).join(' ') + '...',
+          "pubDate": new Date(item.date_published).toUTCString()
+        }
+      });
     }
   }
 
