1754ec60dd60a9635728915ac0cd7a421911d66c
Johan Groenen <johan@jgroenen.nl>
2024-07-09 13:20:59 +0200

    accept rss json too

--- assets/js/modules/rssLoader.js
+++ assets/js/modules/rssLoader.js
@@ -1,18 +1,19 @@
 export default function () {
-  var rssFeed;
-
   async function fetchRssFeed(rssFeedUrl) {
-    var xml = rssFeed = await fetch(rssFeedUrl)
-      .then(response => response.text())
-      .then(str => new window.DOMParser().parseFromString(str, "text/xml"));
-    return Array.from(xml.querySelectorAll("item")).map(item => {
-      return {
-        "title": item.querySelector("title").textContent,
-        "link": item.querySelector("link").textContent,
-        "description": item.querySelector("description").textContent,
-        "pubDate": item.querySelector("pubDate").textContent
-      }
-    });
+    var responseText = await fetch(rssFeedUrl).then(response => response.text());
+    if (responseText.includes("xml version")) {
+      var xml = new window.DOMParser().parseFromString(responseText, "text/xml");
+      return Array.from(xml.querySelectorAll("item")).map(item => {
+        return {
+          "title": item.querySelector("title").textContent,
+          "link": item.querySelector("link").textContent,
+          "description": item.querySelector("description").textContent,
+          "pubDate": item.querySelector("pubDate").textContent
+        }
+      });
+    } else {
+      return JSON.parse(responseText).items;
+    }
   }
 
   return {
